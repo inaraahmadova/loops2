db1c72f29d193cb3ddff3ba4dd88b4900485a636
inaraahmadova <tu7r5hs82@code.edu.az>
2024-03-28 20:37:27 +0400

    first commit

---
+++ loops2/Program.cs
@@ -0,0 +1,491 @@
+﻿//int temperatur = 10;
+//if (temperatur < 15)
+//{
+//    Console.WriteLine("hava soyuqdur");
+//}
+//else
+//{
+//    Console.WriteLine("hava istidir");
+//}
+
+
+
+
+
+//string month = "sentyabr";
+//switch (month)
+//{
+//    case "yanvar":
+//        Console.WriteLine("31 gun var");
+//        break;
+//    case "mart":
+//        Console.WriteLine("31 gun var");
+//        break;
+//    case "may":
+//        Console.WriteLine("31 gun var");
+//        break;
+//    case "iyul":
+//        Console.WriteLine("31 gun var");
+//        break;
+//    case "avqust":
+//        Console.WriteLine("31 gun var");
+//        break;
+//    case "oktyabr":
+//        Console.WriteLine("31 gun var");
+//        break;
+//    case "dekabr":
+//        Console.WriteLine("31 gun var");
+//        break;
+//    case "aprel":
+//        Console.WriteLine("30 gun var");
+//        break;
+//    case "iyun":
+//        Console.WriteLine("30 gun var");
+//        break;
+//    case "sentyabr":
+//        Console.WriteLine("30 gun var");
+//        break;
+//    case "fevral":
+//        Console.WriteLine("28 gun var");
+//        break;
+//}
+
+
+
+
+
+//int num = 6;
+//int total = 1;
+//for (int i = 1; i < <= num; i++)
+//{
+//    total = num * i;
+//}
+//Console.WriteLine(total);
+
+
+
+
+
+
+
+
+
+
+//int num = 7;
+//for (int i = 2; i < num; i++)
+//{
+//    if (num % i == 0)
+//    {
+//        Console.WriteLine("eded murekkebdir");
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("eded sadedir");
+//        break;
+//    }
+//}
+
+
+
+
+
+
+
+//int[] arr = { 5, 3, 4, 2, 9, 8 };
+//int sum = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] % 2 != 0)
+//    {
+//        sum += arr[i]
+//    }
+//}
+//Console.WriteLine(sum);
+
+
+
+
+
+
+
+
+//int[] arr = { 5, 3, 4, 2, 9, 8, 20 };
+//int max = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i > max])
+//    {
+//        max = arr[i];
+//    }
+//}
+//Console.WriteLine(max);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int num = 235;
+//int a = 0;
+//int b = 0;
+//int c = 0;
+//a = num / 100;
+//b = num % 10;
+//c = (num / 10) % 10;
+//if (a != b && a != c && b != c)
+//{
+//    Console.WriteLine("reqemleri beraber deyil");
+//}
+//else
+//{
+//    Console.WriteLine("reqemleri beraberdir");
+//}
+
+
+
+
+
+
+
+
+//int num = 8;
+//int count = 0;
+//while (num % 2 == 0)
+//    num /= 2
+//        if (num / 2 == 1)
+//{
+//    count++;
+//    Console.WriteLine(count);
+//}
+//if (num % 2 != 0)
+//{
+//    Console.WriteLine("eded 2-nin quvveti deyil");
+//}
+
+
+
+
+
+
+
+
+
+//int num = 563;
+//int sum = 0;
+//while (num > 0)
+//{
+//    int eded = num % 10;
+//    if (eded > 0)
+//    {
+//        sum += eded;
+//    }
+//    num /= 10;
+//}
+//Console.WriteLine(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+//int[] arr = { 5, 3, 2, 7, 9, 4, 6, 8 };
+//int min = arr[0];
+//for (int i = 1; i < arr.Length; i++)
+//{
+//    if (arr[i] < min)
+//    {
+//        min = arr[i];
+//    }
+//}
+//Console.WriteLine(min);
+
+
+
+
+
+
+
+
+//int[] arr = { 1, 77, 6, 14, 5 };
+//int count = 0;
+//for (int i = 0; i < length; i++)
+//{
+//    if (arr[i] < 10)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int num = 29;
+//int a = num % 10;
+//int b = num / 10;
+//if (a > b)
+//{
+//    Console.WriteLine("teklikler boyukdur");
+//}
+//else
+//{
+//    Console.WriteLine("onluqlar boyukdur");
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int[] nums = { 2, 100, 4, 1, 56, 13 };
+//int max = 0;
+//int min = 0;
+//for (int i = 1; i < nums.Length; i++)
+//{
+//    if (nums[max] < nums[i])
+//    {
+//        max = i;
+//    }
+//    if (nums[min] > nums[i])
+//    {
+//        min = i
+//    }
+//}
+//Console.WriteLine(max);
+//Console.WriteLine(min);
+
+
+
+
+
+
+
+
+
+
+//int num = 85854;
+//int count = 0;
+//while (num > 0)
+//{
+//    num = num / 10;
+//    count++;
+//}
+//Console.WriteLine(count);
+
+
+
+
+
+
+
+
+//int num = 333;
+//int a = num;
+//int b = 1;
+//while (a > 0)
+//{
+//    a = a / 10;
+//    b++;
+//}
+//num = (1000 * b) + num;
+//Console.WriteLine(num);
+
+
+
+
+
+
+
+
+
+
+//int num = 777;
+//int eded = (num * 10 + 7) * 7 / 100;
+//Console.WriteLine(eded);
+
+
+
+
+
+
+
+
+
+
+//int num = 123;
+//int eded = num * 1000 + num;
+//Console.WriteLine(eded);
+
+
+
+
+
+
+
+
+
+
+//int num = 22;
+//if (num % 2 == 0)
+//{
+//    Console.WriteLine("eded cutdur");
+//}
+//else
+//{
+//    Console.WriteLine("eded tekdir")
+//}
+
+
+
+
+
+
+//int num = 21;
+//if (num % 21 == 0)
+//{
+//    Console.WriteLine("eded 3-e ve 7-e bolunur");
+//}
+
+
+
+
+
+
+
+
+
+
+//int first = 6;
+//int second = 7;
+//if (first > second)
+//{
+//    Console.WriteLine(first);
+//}
+//else
+//{
+//    Console.WriteLine(second);
+//}
+
+
+
+
+
+
+
+
+
+
+
+//string[] arr = { "ab106", "ab107", "ab108" };
+//int count = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] == "ab106")
+//    {
+//        Console.WriteLine("ab106 var");
+//    }
+//    else
+//    {
+//        Console.WriteLine("ab106 yoxdur");
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+//int num = 2;
+//int num2 = 50;
+//int count = 0;
+//for (int i = num; i < num2; i++)
+//{
+//    if (i % 15 == 0)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+
+
+
+
+
+
+
+
+
+
+//int[] arr1 = { 2, 3, 4, 5, 6, 7, 8 };
+//int[] arr2 = { 9, 11, 4, 13, 6, 7, 22 };
+//for (int i = 0; i < arr1.Length; i++)
+//{
+//    for (int j = 0; j < arr2.Length; j++)
+//    {
+//        if (arr1[i] == arr2[j])
+//        {
+//            Console.WriteLine(arr1[i]);
+//        }
+//    }
+//}
+
+
+
+
+
+
+//int[] arr = { 5, 2, 3 };
+//int a = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    for (int j = 0; j < length; j++)
+//    {
+//        a = arr[j];
+//        arr[j] = arr[j + 1];
+//        arr[j + 1] = a;
+//    }
+//}
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    Console.WriteLine(arr[i]);
+//}
